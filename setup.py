a4d378811a3db3350ddc2be24b94e1f36f8da355
eswaryalakanti <ylakshmichandra03@gmail.com>
2024-07-24 18:41:14 +0530

    third commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Eswar Yalakanti',
+    author_email='yalakantieswar@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain-openai"],
+    packages=find_packages()
+)
