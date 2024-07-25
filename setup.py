c882ba375f01c931488da9ece81f2be99975fc00
eswaryalakanti <ylakshmichandra03@gmail.com>
2024-07-25 17:35:27 +0530

    fouth commit

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Eswar Yalakanti',
     author_email='yalakantieswar@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain-openai"],
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain-openai","langchain_community"],
     packages=find_packages()
 )
